8d6b1c2fb19081d2350e365cd60737deee38477a
Leandro David Cacciagioni <leandro.21.2008@gmail.com>
2015-06-04 19:58:35 -0300

    One more and counting

---
+++ algorithms/warmup/serviceLane/servicelane.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# This script will try to solve the problem described in:
+# https://www.hackerrank.com/challenges/service-lane
+
+
+def get_vehicle(lane, start, end):
+    """
+    This function will detect which vehicle can pass all the way from the
+    begining to the exit.
+    """
+    return min(lanes[start:end])
+
+if __name__ == "__main__":
+    n, t = input().split()
+    lanes = input().split()
+    test_cases = list()
+    for i in range(int(t)):
+        test_cases.append(input().split())
+
+    for i in range(int(t)):
+        st = int(test_cases[i][0])
+        en = int(test_cases[i][0])
+        print(get_vehicle(lanes, st, en))
