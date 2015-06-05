6a8f5809e6cd450cf9c0f6980122922a59248c36
Leandro David Cacciagioni <leandro.21.2008@gmail.com>
2015-06-05 14:39:54 -0300

    Added mod fib

---
+++ algorithms/dynamicprogramming/fibonaccimodified/fibonaccimodified.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# This script will try to solve the problem described in:
+# https://www.hackerrank.com/challenges/fibonacci-modified
+from math import pow
+
+def mod_fib(first_term, second_term, limit, step):
+    new_term = 
+
+
+if __name__ == "__main__":
+    ft, st, lim = input().split()
+    mod_fib(ft, st, lim, 0)
