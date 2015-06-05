593ef08a9658187dedc7766d11d519c6ab239d22
Leandro David Cacciagioni <leandro.21.2008@gmail.com>
2015-06-05 15:16:17 -0300

    Added socks

---
+++ algorithms/combinatorics/minimumdraws/minimumdraws.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# This script will try to solve the problem described in:
+# https://www.hackerrank.com/challenges/minimum-draws
+
+
+def min_draw(num_pairs):
+    return num_pairs+1
+
+
+if __name__ == "__main__":
